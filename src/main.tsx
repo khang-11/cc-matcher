b05ea77c77dd594afbcbd0164166b7ea8cd1033f
Khang Nguyen <khang.nguyen11@hotmail.com>
2026-04-20 16:47:49 +1000

    log commit SHA to console on startup

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+console.log(`commit: ${import.meta.env.VITE_COMMIT_SHA ?? 'dev'}`)
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
